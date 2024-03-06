efcc080622357f683bd6463f832965afbbab44f7
atul120212 <atulsharma28092004@gmail.com>
2024-03-06 23:01:48 +0530

    q7

---
+++ q7.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class q4{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        for(int i=4;i>0;i++){
+            for(int j=i;j<0;j++){
+                System.out.print(" ");
+                
+            }
+            System.out.println("*");
+        }
+
+    }
+}
