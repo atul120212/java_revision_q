60b81da0163753a4d493bc2abe4d4c4e1cf52dc6
atul120212 <atulsharma28092004@gmail.com>
2024-11-01 23:57:36 +0530

    maxSubArraySum

---
+++ maxSubarraySum.java
@@ -0,0 +1,29 @@
+
+public class maxSubarraySum {
+
+    public static void maxSubarraySum(int numbers[]) {
+        int currSum;
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < numbers.length; i++) {
+            int start = i;
+            for (int j = 0; j < numbers.length; j++) {
+                int end = j;
+                currSum = 0;
+                for (int k = start; k <= end; k++) {
+                    currSum += numbers[k];
+                }
+                System.out.println(currSum);
+                if (maxSum < currSum) {
+                    maxSum = currSum;
+                }
+            }
+        }
+        System.out.println("maxsum =" + maxSum);
+
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {3, 5, 2, 7, 8, 13};
+        maxSubarraySum(numbers);
+    }
+}
