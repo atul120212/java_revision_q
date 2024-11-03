56c9990b98a52b68239ba230837d4d6224d6d938
atul120212 <atulsharma28092004@gmail.com>
2024-11-03 15:11:02 +0530

    trappedWtare

--- LargestNumber.java
+++ LargestNumber.java
@@ -1,23 +1,24 @@
-import java.util.*;
 
-public class LargestNumber{
-    public static int getLargest(int numbers[]){
+public class LargestNumber {
+
+    public static int getLargest(int numbers[]) {
         int largest = Integer.MIN_VALUE;
         int smallest = Integer.MAX_VALUE;
-        for(int i=0; i<numbers.length; i++){
-            if(largest<numbers[i]){
-                largest=numbers[i];
+        for (int i = 0; i < numbers.length; i++) {
+            if (largest < numbers[i]) {
+                largest = numbers[i];
             }
-            if(smallest>numbers[i]){
-                smallest=numbers[i];
+            if (smallest > numbers[i]) {
+                smallest = numbers[i];
             }
         }
-        
-        System.out.println("smallest value:"+smallest);
+
+        System.out.println("smallest value:" + smallest);
         return largest;
     }
-    public static void main(String args[]){
-        int numbers[] = {1,5,7,2,9,4,3};
-        System.out.println("largest value is:"+getLargest(numbers));
+
+    public static void main(String args[]) {
+        int numbers[] = {1, 5, 7, 2, 9, 4, 3};
+        System.out.println("largest value is:" + getLargest(numbers));
     }
 }
